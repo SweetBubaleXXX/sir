4278e0633a6d60156dd634709c93e63c12fd2363
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-04-12 20:15:13 +0300

    Improved sir function

--- public/script.js
+++ public/script.js
@@ -46,8 +46,8 @@ form.addEventListener('submit', e => {
 function sir(total, sick, speed, period) {
     const sickPoints = [];
     for (let t = 0; t < period; t++) {
-        sick += Math.ceil(((total - sick) * t * sick * speed) / (42 * total));
-        sickPoints.push(sick);
+        sick += Math.ceil(Math.exp(((total - sick) * speed * t) / total));
+        sickPoints.push(Math.min(sick, total));
     }
     return sickPoints;
 }
